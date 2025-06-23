71bde331af9ac275c22af3b3ebac69df6eb93bd9
monisha <monisha200512@gmail.com>
2025-06-23 16:32:29 +0530

    Initial commit: Student Course Management System with Spring Boot

---
+++ src/main/java/com/devops/studentapp/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.devops.studentapp.controller;
+
+import com.devops.studentapp.model.Student;
+import com.devops.studentapp.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+}
---
+++ src/test/java/com/devops/studentapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.devops.studentapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to StudentApp!";
+    }
+}
